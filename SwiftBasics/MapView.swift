1621774f4eaa74c55b25f76898f1e2b032f9438d
Rodion <rodion.prosvirnin@bk.ru>
2020-07-15 19:58:42 +0300

    navigation from the list to the details page.

--- SwiftBasics/MapView.swift
+++ SwiftBasics/MapView.swift
@@ -11,15 +11,15 @@ import MapKit
 
 //The UIViewRepresentable protocol has two requirements you need to add: a makeUIView(context:) method that creates an MKMapView, and an updateUIView(_:context:) method that configures the view and responds to any changes.
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
     
     //method that sets the map view’s region to the correct coordinates to center the map on Turtle Rock.
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011286, longitude: -116.166868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
     }
@@ -27,6 +27,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
